441ea935f7c983b3f41053a7d2a3de5f93a17d9b
Mark Katerberg <katerberg@fastmail.fm>
2018-09-08 14:30:56 -0500

    Adds react and typescript

---
+++ README.md
@@ -0,0 +1,9 @@
+# Minimal Project Start
+
+## Setup
+
+This project was set up with some tutorials:
+
+ - https://www.robinwieruch.de/minimal-react-webpack-babel-setup/
+ - https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
@@ -7,5 +7,22 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist'
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+    { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+    // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+    { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
   }
 };
