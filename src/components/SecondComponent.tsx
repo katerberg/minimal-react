b2159d01173adc22c67de0d497e3025eda631ee5
Emily Kraft <emily.kraft@s01832-mbpr.2d.hq>
2018-09-10 15:50:08 -0500

    Added second component. Woo hoo.

---
+++ src/components/SecondComponent.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+// 'HelloProps' describes the shape of props.
+// // State is never set so we use the '{}' type.
+export class SecondComponent extends React.Component {
+  render() {
+    return <h2>This is my second component. Isn't that cool?</h2>;
+  }
+}
