aefd40bbce452fdb479b666ce5809228a838eee2
Emily Kraft <ek1074@gmail.com>
2018-09-11 16:45:50 -0500

    Update Hello.tsx
    Fixes to Hello.tsx based on code review

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,23 +3,21 @@ import { SecondComponent } from "./SecondComponent";
 
 export interface HelloProps { compiler: string; framework: string; }
 
-function handleClick() {
-        alert("OUCH!");
-    }
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
 
-
-
-
+handleClick() {
+        alert("OUCH!");
+    }
+        
   render() {
     return <div>
       <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
     <SecondComponent numericalReference="second" exclamation="Yipee"/>
     <SecondComponent numericalReference="third" exclamation="Wahoo"/>
     <SecondComponent numericalReference="fourth" exclamation="Totally tubular"/>
-  <button onClick={()=> handleClick()}>Click me!</button>
+  <button onClick={handleClick}>Click me!</button>
 </div>;
   }
 }
