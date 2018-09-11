a073a1cf32f3cd2e5ce130fb16b83f1a6018b71e
Emily Kraft <emily.kraft@s01832-mbpr.2d.hq>
2018-09-11 13:07:09 -0500

    added customization of second component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,13 +3,24 @@ import { SecondComponent } from "./SecondComponent";
 
 export interface HelloProps { compiler: string; framework: string; }
 
+function handleClick() {
+        alert("OUCH!");
+    }
 // 'HelloProps' describes the shape of props.
 // // State is never set so we use the '{}' type.
+
 export class Hello extends React.Component<HelloProps, {}> {
+
+
+
+
   render() {
     return <div>
       <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
-    <SecondComponent />
-    </div>;
+    <SecondComponent numericalReference="second" exclamation="Yipee"/>
+    <SecondComponent numericalReference="third" exclamation="Wahoo"/>
+    <SecondComponent numericalReference="fourth" exclamation="Totally tubular"/>
+  <button onClick={()=> handleClick()}>Click me!</button>
+</div>;
   }
 }
--- src/components/SecondComponent.tsx
+++ src/components/SecondComponent.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
+export interface SecondComponentProps { numericalReference: string; exclamation: string; }
+
 // 'HelloProps' describes the shape of props.
 // // State is never set so we use the '{}' type.
-export class SecondComponent extends React.Component {
+export class SecondComponent extends React.Component<SecondComponentProps, {}>  {
   render() {
-    return <h2>This is my second component. Isn't that cool?</h2>;
+    return <h2>This is my {this.props.numericalReference} component. {this.props.exclamation}!</h2>;
   }
 }
