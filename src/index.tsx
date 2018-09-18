79b7a9fdb4f3c55bf942b9659130bf54286b5c46
Jose Berrio <aldarisbm@gmail.com>
2018-09-17 23:35:24 -0500

    kinda sorta fixed the img-webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
 import { Hello } from "./components/Hello";
 import './index.scss';
 
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
+const logo = require('./logo.jpg');
+
+
+ReactDOM.render(<div>
+  <img src={logo}/>
+  <Hello compiler="TypeScript" framework="React" /></div>,
     document.getElementById("app")
 );
