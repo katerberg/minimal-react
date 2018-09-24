3db731357d4d2ce20d6d4cefdf3c664c9aee0612
Mark Katerberg <katerberg@fastmail.fm>
2018-09-24 14:16:28 -0500

    tested letter component

---
+++ src/components/Letter.test.tsx
@@ -0,0 +1,18 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import { expect } from 'chai';
+import { PeopleList } from './PeopleList';
+import { Letter } from './Letter';
+
+describe('Letter', () => {
+	test ('Shows letters larger than names', () => {
+		const letter = shallow(<Letter letter='Q' names={['Quincy']}/>);
+		expect(letter.contains(<h1>Q</h1>)).to.equal(true);
+		expect(letter.contains(<li key='Quincy'>Quincy</li>)).to.equal(true);
+	})
+	test ('Displays no list when there are no names for that letter', () => {
+		const letter = shallow(<Letter letter='Q' names={[]}/>);
+		expect(letter.contains(<h1>Q</h1>)).to.equal(true);
+		expect(letter.contains(<ul></ul>)).to.equal(true);
+	})
+});
