557ed3152881cc1df100f7e20aceb9ee7f9bccdc
Kelsey Holt <kelseyjo959@gmail.fm>
2018-09-27 09:54:59 -0500

    added fn for brew check, fixed spacing, clarified fn name is_sudo()

--- installation-dependencies/mongo-setup.sh
+++ installation-dependencies/mongo-setup.sh
@@ -9,12 +9,20 @@ is_mongo_installed () {
   return 1
 }
 
-is_privileged () {
-if [ "$(id -u)" -ne 0 ]; then
-  return 1
-fi
-return 0
+is_sudo () {
+  if [ "$(id -u)" -ne 0 ]; then
+    return 1
+  fi
+  return 0
 }  
+
+is_brew_installed () {
+  command -v brew > /dev/null
+  if [ $? -eq 0 ]; then
+    return 0
+  fi
+  return 1
+}
 
 install_mongo () {
   brew update
@@ -31,7 +39,13 @@ if [ $? -eq 0 ]; then
   exit 0
 fi
 
-is_privileged
+is_brew_installed
+if [ $? -ne 0 ]
+  echo "you need to install brew before you can continue!"
+  exit 1
+fi
+
+is_sudo
 if [ $? -ne 0 ]; then
   echo "$0 requires escalated priviliges to run"
   exit 1
@@ -44,8 +58,8 @@ if [ $? -ne 0 ]; then
   echo "failed to install mongo"
   exit 1
 fi
-# i am confused on where I should be putting these echos. I only want them to print if mongo was successfully installed
+
 echo "mongo was installed"
-echo "use brew services start mongodb to begin mongod process"
-echo "use brew services stop mongodb to end mongod process"
-echo "use mongo to access the mongo shell"
+echo "use 'brew services start mongodb' to begin mongod process"
+echo "use 'brew services stop mongodb' to end mongod process"
+echo "use 'mongo' to access the mongo shell"
