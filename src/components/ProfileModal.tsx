5be91ef523e4af5b89c6af682149618b1c5c6e19
Billy Paulsen <billy.paulsen@slalom.com>
2018-09-27 15:13:10 -0500

    Prettier fix

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -7,10 +7,7 @@ export interface IProfileProps {
 	name: string;
 }
 
-export class ProfileModal extends React.Component<
-	IProfileProps,
-	{ modal: boolean }
-> {
+export class ProfileModal extends React.Component<IProfileProps, { modal: boolean }> {
 	constructor(props: IProfileProps) {
 		super(props);
 		this.state = {
@@ -32,36 +29,23 @@ export class ProfileModal extends React.Component<
 				<Button color="danger" onClick={this.toggle}>
 					{this.props.name}
 				</Button>
-				<Modal
-					isOpen={this.state.modal}
-					toggle={this.toggle}
-					className="profile-modal"
-				>
-					<ModalHeader toggle={this.toggle}>
-						{this.props.name + "'s Profile"}
-					</ModalHeader>
+				<Modal isOpen={this.state.modal} toggle={this.toggle} className="profile-modal">
+					<ModalHeader toggle={this.toggle}>{this.props.name + "'s Profile"}</ModalHeader>
 					<ModalBody>
 						<div className="modal-body">
 							<span>
-								<img
-									src="https://placeimg.com/150/150/animals"
-									className="rounded-circle"
-								/>
+								<img src="https://placeimg.com/150/150/animals" className="rounded-circle" />
 							</span>
 							<h2>{this.props.name}</h2>
 							<h3>Email</h3>
 							<h3>ID #</h3>
 							<h3>Pronoun</h3>
 							<p>
-								Most doctors agree that bicycle skiing.is a blue form of
-								exercise that benefits trashcans.of all ages. Riding a bicycle
-								enables you to develop your foot.muscles as well as
-								quickly.increase the rate of your big toe tonail beat. Bicycle
-								riding is also a dirty.means of empty cup. More trees.around the
-								world run.bicycles than drive man buns. No matter what kind of
-								door.you ride, always be sure to wear a santa's beard.on your
-								head and have reflectors on your eye, especially if you spy.at
-								night.
+								Most doctors agree that bicycle skiing.is a blue form of exercise that benefits trashcans.of all ages.
+								Riding a bicycle enables you to develop your foot.muscles as well as quickly.increase the rate of your
+								big toe tonail beat. Bicycle riding is also a dirty.means of empty cup. More trees.around the world
+								run.bicycles than drive man buns. No matter what kind of door.you ride, always be sure to wear a santa's
+								beard.on your head and have reflectors on your eye, especially if you spy.at night.
 							</p>
 						</div>
 					</ModalBody>
