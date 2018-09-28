674a000a37deb5d3747ed86a1062251c1e8ada4f
Billy Paulsen <billy.paulsen@slalom.com>
2018-09-28 10:33:23 -0500

    Added test for validateForm disabling submit button

--- src/components/LoginForm.test.tsx
+++ src/components/LoginForm.test.tsx
@@ -8,13 +8,21 @@ describe('LoginForm tests', () => {
 		test('returns true if there are errors', () => {
 			const loginForm: ShallowWrapper = shallow(<LoginForm />);
 			const loginFormInstance : any = loginForm.instance();
-			console.log(loginFormInstance instanceof LoginForm);
 			const inputState = { email: '', password: '', errorMessages: ['Error1','Error2'] };
-			spyOn(loginFormInstance, 'validateForm').and.callThrough();
+			jest.spyOn(loginFormInstance, 'validateForm');
 
 			const result : boolean = loginFormInstance.validateForm(inputState);
 
 			expect(result).to.eql(true);
+		});
+		test('Button enables if email and password are correct length', () => {
+			const loginForm: ShallowWrapper = shallow(<LoginForm />);
+			const loginFormInstance : any = loginForm.instance();
+			loginFormInstance.setState({ email: 'billy@email.com', password: 'fakepass' });
+			
+			loginFormInstance.validateForm(loginFormInstance.state);
+
+			expect(loginFormInstance.state.buttonDisable).to.eql(false);
 		});
 	});
 });
