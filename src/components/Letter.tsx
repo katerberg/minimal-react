fc11ce186dfca2021494c0f19648ba6f0074ff8e
Emily Kraft <ek1074@gmail.com>
2018-10-01 17:18:28 -0500

    fixing Employee to IEmployee

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { Employee } from './PeopleList';
+import { IEmployee } from './PeopleList';
 import { ProfileModal } from './ProfileModal';
 
 export interface ILetterProps {
 	letter: string;
-	employees: Employee[];
+	employees: IEmployee[];
 }
 
 export class Letter extends React.Component<ILetterProps, {}> {
@@ -14,7 +14,7 @@ export class Letter extends React.Component<ILetterProps, {}> {
 				<h1>{this.props.letter}</h1>
 				<hr />
 				<ul>
-					{this.props.employees.map((employee: Employee) => (
+					{this.props.employees.map((employee: IEmployee) => (
 						<li key={employee._id}>
 							<ProfileModal name={employee.name} />
 						</li>
--- src/components/PeopleList.test.tsx
+++ src/components/PeopleList.test.tsx
@@ -2,18 +2,18 @@ import { expect } from 'chai';
 import { shallow, ShallowWrapper } from 'enzyme';
 import * as React from 'react';
 import { Letter } from './Letter';
-import { Employee, PeopleList } from './PeopleList';
+import { IEmployee, PeopleList } from './PeopleList';
 
 describe('PeopleList', () => {
 	describe('filterNames()', () => {
 		test('Filters names by letter test', () => {
-			const employeeToFind: Employee = {
+			const employeeToFind: IEmployee = {
 				_id: '5bad03ddcf3fa4426a048ce7',
 				email: 'adrienne.costello@slalom.com (Adrienne Costello)',
 				name: 'Adrienne Costello',
 				phone: '',
 			};
-			const name: Employee[] = [
+			const name: IEmployee[] = [
 				{
 					_id: '5bad03ddcf3fa4426a048ce6',
 					email: 'adam.etchason@slalom.com (Adam Etchason)',
@@ -25,58 +25,58 @@ describe('PeopleList', () => {
 			const letter: string = 'A';
 			const peopleList: PeopleList = new PeopleList({});
 
-			const filteredNames: Employee[] = peopleList.filterNames(name, letter);
+			const filteredNames: IEmployee[] = peopleList.filterNames(name, letter);
 
 			expect(filteredNames).to.contain(employeeToFind);
 			expect(filteredNames.length).to.eql(1);
 		});
 
 		test('Alphabetizes names after filtering', () => {
-			const adrienne: Employee = {
+			const adrienne: IEmployee = {
 				_id: '5bad03ddcf3fa4426a048ce7',
 				email: 'adrienne.costello@slalom.com (Adrienne Costello)',
 				name: 'Adrienne Costello',
 				phone: '',
 			};
-			const adam: Employee = {
+			const adam: IEmployee = {
 				_id: '5bad03ddcf3fa4426a048ce6',
 				email: 'adam.etchason@slalom.com (Adam Etchason)',
 				name: 'Adam Etchason',
 				phone: '',
 			};
-			const employees: Employee[] = [adrienne, adam];
+			const employees: IEmployee[] = [adrienne, adam];
 
 			const letter: string = 'A';
 			const peopleList: PeopleList = new PeopleList({});
 
-			const filteredNames: Employee[] = peopleList.filterNames(employees, letter);
+			const filteredNames: IEmployee[] = peopleList.filterNames(employees, letter);
 			expect(filteredNames).to.eql([adam, adrienne]);
 		});
 
 		test('Does not rearrange people with the same name', () => {
-			const adam: Employee = {
+			const adam: IEmployee = {
 				_id: '1',
 				email: 'adam.etchason@slalom.com (Adam Etchason)',
 				name: 'Adam Etchason',
 				phone: '',
 			};
-			const otherAdam: Employee = {
+			const otherAdam: IEmployee = {
 				_id: '2',
 				email: 'other.adam.etchason@slalom.com (Adam Etchason)',
 				name: 'Adam Etchason',
 				phone: '',
 			};
-			const employees: Employee[] = [otherAdam, adam];
+			const employees: IEmployee[] = [otherAdam, adam];
 
 			const letter: string = 'A';
 			const peopleList: PeopleList = new PeopleList({});
 
-			const filteredNames: Employee[] = peopleList.filterNames(employees, letter);
+			const filteredNames: IEmployee[] = peopleList.filterNames(employees, letter);
 			expect(filteredNames).to.eql([otherAdam, adam]);
 		});
 
 		test('Handles Capitalization', () => {
-			const name: Employee[] = [
+			const name: IEmployee[] = [
 				{
 					_id: '5bad03ddcf3fa4426a048ce6',
 					email: 'adam.etchason@slalom.com (Adam Etchason)',
@@ -93,7 +93,7 @@ describe('PeopleList', () => {
 			const letter: string = 'A';
 			const peopleList: PeopleList = new PeopleList({});
 
-			const filteredNames: Employee[] = peopleList.filterNames(name, letter);
+			const filteredNames: IEmployee[] = peopleList.filterNames(name, letter);
 
 			expect(filteredNames).to.eql([
 				{
@@ -112,49 +112,49 @@ describe('PeopleList', () => {
 		});
 
 		test('Handles multiword first names', () => {
-			const adrienne: Employee = {
+			const adrienne: IEmployee = {
 				_id: '5bad03ddcf3fa4426a048ce7',
 				email: 'adrienne.costello@slalom.com (Adrienne Costello)',
 				name: 'Adrienne May Costello',
 				phone: '',
 			};
-			const adam: Employee = {
+			const adam: IEmployee = {
 				_id: '5bad03ddcf3fa4426a048ce6',
 				email: 'adam.etchason@slalom.com (Adam Etchason)',
 				name: 'Adam Etchason',
 				phone: '',
 			};
-			const amy: Employee = {
+			const amy: IEmployee = {
 				_id: '1bad03ddcf3fa4426a048ce6',
 				email: 'adam.etchason@slalom.com (Adam Etchason)',
 				name: 'Amy Etchason',
 				phone: '',
 			};
-			const employees: Employee[] = [amy, adrienne, adam];
+			const employees: IEmployee[] = [amy, adrienne, adam];
 
 			const letter: string = 'A';
 			const peopleList: PeopleList = new PeopleList({});
 
-			const filteredNames: Employee[] = peopleList.filterNames(employees, letter);
+			const filteredNames: IEmployee[] = peopleList.filterNames(employees, letter);
 
 			expect(filteredNames).to.eql([adam, adrienne, amy]);
 		});
 
 		test('Handles missing names', () => {
-			const employeeToFind: Employee = {
+			const employeeToFind: IEmployee = {
 				_id: '5bad03ddcf3fa4426a048ce7',
 				email: 'adrienne.costello@slalom.com (Adrienne Costello)',
 				name: 'Adrienne Costello',
 				phone: '',
 			};
-			const name: Employee[] = [
+			const name: IEmployee[] = [
 				{ _id: '5bad03ddcf3fa4426a048ce6', name: '', phone: '', email: 'adam.etchason@slalom.com (Adam Etchason)' },
 				employeeToFind,
 			];
 			const letter: string = 'A';
 			const peopleList: PeopleList = new PeopleList({});
 
-			const filteredNames: Employee[] = peopleList.filterNames(name, letter);
+			const filteredNames: IEmployee[] = peopleList.filterNames(name, letter);
 
 			expect(filteredNames).to.eql([employeeToFind]);
 		});
